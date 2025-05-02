dd9dda6e07994fe5c73c4d1b5629e126f80ae20e
qchaille <qchaille@cross-systems.com>
2025-05-02 15:20:49 +0200

    feat(kata-fizzbuzz) : Solution kata fzzbuzz

--- 01-fizzbuzz/src/main/java/org/example/FizzBuzz.java
+++ 01-fizzbuzz/src/main/java/org/example/FizzBuzz.java
@@ -1,8 +1,25 @@
 package org.example;
 
 public class FizzBuzz {
+    public static final int FIZZ_DIVISOR = 3;
+    public static final int BUZZ_DIVISOR = 5;
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
 
-    public int convert(int i) {
-        return 1;
+    public String convert(int input) {
+        if(isAMultipleOf(input, FIZZ_DIVISOR) && isAMultipleOf(input, BUZZ_DIVISOR)) {
+            return FIZZ + BUZZ;
+        }
+        if(isAMultipleOf(input, FIZZ_DIVISOR)) {
+            return FIZZ;
+        }
+        if(isAMultipleOf(input, BUZZ_DIVISOR)) {
+            return BUZZ;
+        }
+        return String.valueOf(input);
+    }
+
+    private static boolean isAMultipleOf(int input, int multiplier) {
+        return input % multiplier == 0;
     }
 }
