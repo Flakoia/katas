5edb1adb235300ccba631d7c29061e3f3b4f36c0
qchaille <qchaille@cross-systems.com>
2025-07-18 17:50:06 +0200

    test(bank-kata) : Acceptance criteria

--- 08-bank-kata/src/main/java/org/example/Account.java
+++ 08-bank-kata/src/main/java/org/example/Account.java
@@ -3,4 +3,11 @@ package org.example;
 public class Account {
 
 
+    public void deposit(Integer amount) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void withdraw(Integer amount) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
