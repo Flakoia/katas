350604893af769913f3f7c210c4d457fe57ed908
qchaille <qchaille@cross-systems.com>
2025-07-23 11:11:48 +0200

    feat(bank-kata) : live code

--- 08-bank-kata/src/main/java/org/example/Account.java
+++ 08-bank-kata/src/main/java/org/example/Account.java
@@ -1,6 +1,21 @@
 package org.example;
 
 public class Account {
+    private final TransactionRepository transactionRepository;
 
+    public Account(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
 
+    public void deposit(Integer amount) {
+        transactionRepository.addDeposit(amount);
+    }
+
+    public void withdraw(Integer amount) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void printStatement() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
